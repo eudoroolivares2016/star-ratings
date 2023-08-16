8c2567d2e558b2fd5b5813aaeb2136b74374293f
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-08-16 15:49:00 -0400

    refactor to have useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import StarRating from './components/StarRating';
+import StarRating from './components/StarRating'
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,40 +1,29 @@
 import React from 'react'
 
 import { useState } from 'react'
-import {FaStar} from 'react-icons/fa'
+import { FaStar } from 'react-icons/fa'
 
 function StarRating({ totalStars = 5 }) {
-    
-    const [selected, setSelected] = useState(true)
-    
-    const getColor = () => {
-        if (selected){
-            return 'red'
-        }
-        else {
-            return 'grey'
-        }
-    }
-    
-    
-    const changeColor = () => {
-        setSelected(!selected)
-    }
 
-    const Star = () => {
-        console.log('Start render was called')
-        return (<FaStar color={getColor()} onClick={changeColor}></FaStar>)
+    const [selectedStars, setSelectedStars] = useState(3)
 
+    // onSelect is just going to default to a function that returns its input
+    const Star = ({ selected = false, onSelect = f => f }) => {
+        return (<FaStar color={selected ? "red" : "grey"} onClick={onSelect}></FaStar>)
     }
 
     const createArray = length => [...Array(length)]
 
-  return (
-    <>
-   { createArray(totalStars).map((n, i) => <Star key={i} />)}
-    {}
-    </>
-  )
+    return (
+        <>
+            {
+            createArray(totalStars).map((n, i) => <Star key={i} onSelect={() => {
+                setSelectedStars(i + 1)
+            }} 
+            selected={selectedStars > i} starId={i} />)}
+        <p> {selectedStars} of {totalStars} stars </p>
+        </>
+    )
 }
 
 export default StarRating
